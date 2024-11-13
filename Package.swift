eaed08a02b82576296b8a60b51ecf1e938d555eb
Ven <ven.wu@baifu-tech.net>
2024-11-13 14:08:59 +0800

    [Feature] 更新 HTTPType 版本至 1.0.2

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["HTTPTypeAsyncHttpClient"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/coollazy/HTTPType.git", from: "1.0.0"),
+        .package(url: "https://github.com/coollazy/HTTPType.git", from: "1.0.2"),
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.23.1"),
     ],
     targets: [
